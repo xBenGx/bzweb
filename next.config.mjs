89f40366c1e7f51966fa2715159b5aaada461679
xBenGx <balfaroy2.0@gmail.com>
2026-01-29 19:48:02 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Tu configuración de imágenes original (Permite todo)
   images: {
     remotePatterns: [
       {
@@ -9,12 +8,8 @@ const nextConfig = {
       },
     ],
   },
-  // ESTO ES CLAVE: Le dice a Hostinger que ignore errores menores de tipos y construya la web igual
   typescript: {
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
 };
 
